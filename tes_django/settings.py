588ee2ae24b6f1402b471b1da4e851784ec95630
Ogi Febrian Saputra <83199832+Dejavu20@users.noreply.github.com>
2025-07-04 20:25:37 +0800

    Konfigurasi Railway 1

--- tes_django/settings.py
+++ tes_django/settings.py
@@ -84,7 +84,7 @@ DATABASES = {
         'NAME': 'db.123',
         'USER': 'root',
         'PASSWORD': '',
-        'HOST': 'localhost',
+        'HOST': '127.0.0.1',
         'PORT': '3306',
     }
 }
